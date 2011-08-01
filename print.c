f483fd7833ae86e3b6071e95c507c3b39db7b505
Malcolm Parsons <mparsons@linguamatics.com>
2011-08-01 13:50:05 +0100

    Fix typo

--- print.c
+++ print.c
@@ -8,7 +8,7 @@ int scm_write(FILE *out, scm_object object) {
     /* Cast the fixnum's value to long because we need to
      * specify the format string yet we don't know the size
      * of a fixnum's value here. Casting to long cannot
-     * loose information so the cast is safe (i.e. won't
+     * lose information so the cast is safe (i.e. won't
      * print an incorrect value.)
      */
     result = fprintf(out, "%ld",
